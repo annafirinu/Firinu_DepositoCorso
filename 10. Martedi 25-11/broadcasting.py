89803aa16b3a88a625d9eb7be928f265bbbde930
annafirinu <anna.firinu2@outlook.it>
2025-11-25 17:03:38 +0100

    broadcasting teoria
    broadcasting teoria

---
+++ 10. Martedi 25-11/broadcasting.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+scalar = 10
+
+# Broadcasting aggiunge lo scalare a ogni elemento dell'array
+result = arr + scalar
+print(result) # Output: [11 12 13 14]
