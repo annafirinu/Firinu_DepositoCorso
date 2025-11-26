919fb1ab7c0c81c46f8937aad9f386940cb7541e
annafirinu <anna.firinu2@outlook.it>
2025-11-26 14:30:51 +0100

    inserimento dati

--- 11. Mercoledi 26-11/main.py
+++ 11. Mercoledi 26-11/main.py
@@ -30,3 +30,11 @@ prezzo REAL
 """
 cursor.execute(sql_create_table)
 print("Tabella 'libri' creata con successo.")
+
+# 4. INSERIMENTO DATI (INSERT) - Metodo Singolo
+# NOTA: Uso '?' come placeholder per sicurezza (evita SQL Injection)
+sql_insert = "INSERT INTO libri (titolo, autore, anno, prezzo) VALUES (?, ?, ?, ?)"
+dati_libro = ("Il Signore degli Anelli", "J.R.R. Tolkien", 1954, 25.50)
+
+cursor.execute(sql_insert, dati_libro)
+print("Inserito un singolo libro.")
